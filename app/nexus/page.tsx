7a7bd96f3585211d59199ca7ac31a512d88c5b0f
roeik18-dotcom <roeik18@gmail.com>
2026-04-22 10:11:15 +0300

    fix nexus route

--- app/nexus/page.tsx
+++ app/nexus/page.tsx
@@ -1,14 +1,7 @@
-"use client";
-
-import GlobeView from "../globe/GlobeView";
-import { useGraphData } from "../graph/useGraphData";
-
-export default function NexusPage() {
-  const data = useGraphData();
-
+export default function Nexus() {
   return (
-    <div style={{ width: "100vw", height: "100vh", background: "#000" }}>
-      <GlobeView data={data} />
+    <div style={{padding:40,color:"#fff",background:"#000",minHeight:"100vh"}}>
+      <h1>NEXUS WORKING</h1>
     </div>
   );
 }
